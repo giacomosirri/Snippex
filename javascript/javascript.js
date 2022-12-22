17a89a23430630a992b0973df5bf6d824fed0ce6
Andrea Zavatta <zavattaandrea@gmail.com>
2022-12-22 13:44:42 +0100

    fix bug

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,14 @@
 
-function changeText(){
-    let label = document.querySelector(".post-content label");
-    let checkbox = document.querySelector(".post-content input[type=checkbox]");
+function changeText(label){
+    let paragraph = label.previousElementSibling
+    let checkbox = paragraph.previousElementSibling;
+    console.log(checkbox);
+    console.log(paragraph);
     if(checkbox.checked){
+        paragraph.style.webkitLineClamp = "3";
         label.innerHTML = "read more";
     }else{
+        paragraph.style.webkitLineClamp = "unset";
         label.innerHTML = "read less";
     }
 }
