07bc801190821646acb5b58f468a402ed83087eb
Andrea Zavatta <zavattaandrea@gmail.com>
2022-12-22 14:25:27 +0100

    remove unused console log

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,8 +2,6 @@
 function changeText(label){
     let paragraph = label.querySelector('p');
     let checkbox = label.previousElementSibling;
-    console.log(checkbox);
-    console.log(paragraph);
     if(checkbox.checked){
         paragraph.style.webkitLineClamp = "4";
     }else{
