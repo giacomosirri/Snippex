f87426d3f61ae4aa769a1a6d5afdf0a2d288c231
Andrea Zavatta <zavattaandrea@gmail.com>
2022-12-22 14:14:16 +0100

    create new animation for visualizing extended post

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,14 +1,12 @@
 
 function changeText(label){
-    let paragraph = label.previousElementSibling
-    let checkbox = paragraph.previousElementSibling;
+    let paragraph = label.querySelector('p');
+    let checkbox = label.previousElementSibling;
     console.log(checkbox);
     console.log(paragraph);
     if(checkbox.checked){
         paragraph.style.webkitLineClamp = "3";
-        label.innerHTML = "read more";
     }else{
         paragraph.style.webkitLineClamp = "unset";
-        label.innerHTML = "read less";
     }
 }
