b20c37c2d21279bfe33b433555021a64208d1452
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-12-22 14:57:53 +0100

    Make profile page mobile first and add animation on plus button

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,12 +1,33 @@
-
-function changeText(label){
+function changeText(label) {
     let paragraph = label.querySelector('p');
     let checkbox = label.previousElementSibling;
-    console.log(checkbox);
-    console.log(paragraph);
-    if(checkbox.checked){
+    if(checkbox.checked) {
         paragraph.style.webkitLineClamp = "4";
-    }else{
+    } else {
         paragraph.style.webkitLineClamp = "unset";
+    }
+}
+
+function showRatingCategories(index) {
+    const div = document.getElementsByClassName("post-interactions")[index];
+    const icons = div.children;
+    for (let i=0; i<icons.length; i++) {
+        if(icons[i].className==="rate-post") {
+            icons[i].style.display = "none";
+        } else {
+            icons[i].style.display = "inline";
+        }
+    }
+}
+
+function showPlus(index) {
+    const div = document.getElementsByClassName("post-interactions")[index];
+    const icons = div.children;
+    for (let i=0; i<icons.length; i++) {
+        if(icons[i].className==="rate-post" || icons[i].className==="comment-post") {
+            icons[i].style.display = "inline";
+        } else {
+            icons[i].style.display = "none";
+        }
     }
 }
