d65b2818fcab96e5c7dfc167789605ee726acb70
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-01-08 17:41:06 +0100

    Fix error in servername

--- javascript/userprofile.js
+++ javascript/userprofile.js
@@ -1,21 +1,19 @@
 function addBasicInfo(data) {
     return `
-            <div class="row">
-            <div class="col-12 d-flex justify-content-center">
-                <div class="d-flex justify-content-start flex-column">
-                    <h1 style="color: black">${data["name"]} ${data["surname"]}</h1>
-                    <h2 style="color: black; font-size: 18px; margin-top: -14px">~${data["username"]}</h2>
-                    <div style="margin-bottom: 5px">
-                        <img id="profile-pic" src="../img/${data["profilePic"]}.jpg" alt="profile pic"/>
-                    </div>
+        <div class="col-10 d-flex justify-content-center">
+            <div class="d-flex justify-content-start flex-column">
+                <h1 style="color: black">${data["name"]} ${data["surname"]}</h1>
+                <h2 style="color: black; font-size: 18px; margin-top: -14px">~${data["username"]}</h2>
+                <div style="margin-bottom: 5px">
+                    <img id="profile-pic" src="../img/${data["profilePic"]}.jpg" alt="profile pic"/>
                 </div>
             </div>
         </div>
     `;
 }
 
-axios.get('php/userprofile-api.php').then(response => {
+axios.get('../php/userprofile-api.php').then(response => {
     const profileHeader = addBasicInfo(response.data);
-    const header = document.querySelector("main>header");
+    const header = document.querySelector("main>header>div");
     header.innerHTML += profileHeader;
 });
--- php/userprofile-api.php
+++ php/userprofile-api.php
@@ -1,6 +1,6 @@
 <?php
 require_once "bootstrap.php";
-$dbh = new DatabaseHelper("mysqli", "root", "", "snippex", 3306);
+$dbh = new DatabaseHelper("localhost", "root", "", "snippex", 3306);
 
 $user_data = $dbh->getUserData("jaaack");
 echo json_encode($user_data);
