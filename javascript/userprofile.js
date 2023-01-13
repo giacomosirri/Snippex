1f8c160ada57088083ba08aca08d4eafa3c74b06
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-01-13 15:52:05 +0100

    Fix bug in userprofile.js

--- javascript/userprofile.js
+++ javascript/userprofile.js
@@ -2,10 +2,10 @@ function addBasicInfo(data) {
     return `
         <div class="col-10 d-flex justify-content-center">
             <div class="d-flex justify-content-start flex-column">
-                <h1 style="color: black">${data["name"]} ${data["surname"]}</h1>
-                <h2 style="color: black; font-size: 18px; margin-top: -14px">~${data["username"]}</h2>
+                <h1 style="color: black">${data[0]["Name"]} ${data[0]["Surname"]}</h1>
+                <h2 style="color: black; font-size: 18px; margin-top: -14px">~${data[0]["Username"]}</h2>
                 <div style="margin-bottom: 5px">
-                    <img id="profile-pic" src="../img/${data["profilePic"]}.jpg" alt="profile pic"/>
+                    <img id="profile-pic" src="../profile_pics/${data[0]["ProfilePic"]}" alt="profile pic"/>
                 </div>
             </div>
         </div>
--- php/bootstrap.php
+++ php/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
 session_start();
-const UPLOAD_DIR = "../profile_pics/";
 require_once "../database/database.php";
 ?>
