9e10a10c25857144739bca70e84bd2974f38dbba
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-01-13 19:41:48 +0100

    Retrieve title of a generic user's page from GET

--- php/userprofile.php
+++ php/userprofile.php
@@ -1,5 +1,13 @@
 <?php
 require_once "bootstrap.php";
+global $dbh;
+
 $templateParams["js"] = array("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", "../javascript/userprofile.js");
+if (isset($_GET["Username"])) {
+    $templateParams["title"] = $_GET["Username"]."'s profile";
+} else {
+    $templateParams["title"] = "Generic title";
+}
+
 require "../html/userprofile.html";
 ?>
