0f793ebb7771ae9dff782044c076459bb8e0f7d4
Andrea Zavatta <zavattaandrea@gmail.com>
2023-01-18 16:18:57 +0100

    add some query

--- database/database.php
+++ database/database.php
@@ -18,5 +18,28 @@ class DatabaseHelper {
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
+
+    public function getRatingCategory() {
+        $stmt = $this->db->prepare("SELECT * FROM rating_category");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getPostFromUser($username) {
+        $stmt = $this->db->prepare("SELECT * FROM post WHERE username = ?");
+        $stmt->bind_param('s',$username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getCommentFromPost($post_id) {
+        $stmt = $this->db->prepare("SELECT * FROM comment WHERE post_id = ?");
+        $stmt->bind_param('s',$post_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
 }
 ?>
--- php/userprofile-api.php
+++ php/userprofile-api.php
@@ -1,7 +1,6 @@
 <?php
 require_once "bootstrap.php";
 global $dbh;
-
 $user_data = $dbh->getUserData("jaaack");
 header("Content-Type: application/json");
 echo json_encode($user_data);
