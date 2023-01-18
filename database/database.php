cf753bab50f70e40fd192debc907ecaf615feb69
Andrea Zavatta <zavattaandrea@gmail.com>
2023-01-18 16:26:16 +0100

    fix bug and add function

--- database/database.php
+++ database/database.php
@@ -19,15 +19,15 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getRatingCategory() {
-        $stmt = $this->db->prepare("SELECT * FROM rating_category");
+    public function getRating() {
+        $stmt = $this->db->prepare("SELECT * FROM rating");
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
     public function getPostFromUser($username) {
-        $stmt = $this->db->prepare("SELECT * FROM post WHERE username = ?");
+        $stmt = $this->db->prepare("SELECT * FROM post WHERE Writer = ?");
         $stmt->bind_param('s',$username);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -35,8 +35,16 @@ class DatabaseHelper {
     }
 
     public function getCommentFromPost($post_id) {
-        $stmt = $this->db->prepare("SELECT * FROM comment WHERE post_id = ?");
+        $stmt = $this->db->prepare("SELECT * FROM comment WHERE Post = ?");
         $stmt->bind_param('s',$post_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getPointsFromUser($username){
+        $stmt = $this->db->prepare("SELECT * FROM points WHERE User = ?");
+        $stmt->bind_param('s',$username);
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
