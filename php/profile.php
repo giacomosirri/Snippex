15d112445be837cf2bbe1b9edc187300121d61c3
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-01-20 10:29:14 +0100

    Automatize personal profile page

--- php/profile.php
+++ php/profile.php
@@ -4,11 +4,11 @@ require_once "bootstrap.php";
 $templateParams["js"] = array("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", "../javascript/userprofile.js");
 if (isset($_GET["Username"])) {
     $templateParams["user"] = $_GET["Username"];
-    $templateParams["title"] = $templateParams["user"]."'s profile";
 } else {
     throw new Error("Something went wrong!");
 }
-$templateParams["page"] = "../template/userprofile.html";
+$templateParams["title"] = "Your profile";
+$templateParams["page"] = "../template/profile.html";
 
 require "./base.php";
 ?>
