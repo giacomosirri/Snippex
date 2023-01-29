b949e6d60c7f444e00ee3dc362b4673d8c84dfa9
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-01-29 19:33:47 +0100

    Fix bug in userprofile.php

--- php/userprofile.php
+++ php/userprofile.php
@@ -2,7 +2,7 @@
 session_start();
 $templateParams["js"] = array("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", "../javascript/userprofile.js", "../javascript/image.js");
 if (isset($_GET["Username"])) {
-    $templateParams["user"] = $_SESSION["LoggedUser"];
+    $templateParams["user"] = $_GET["Username"];
     $templateParams["title"] = $templateParams["user"]."'s profile";
 } else {
     throw new Error("Something went wrong!");
