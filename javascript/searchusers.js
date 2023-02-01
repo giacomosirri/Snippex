cb551934e0b51a75ca6b196a8e7b58930e56d6f2
Andrea Zavatta <zavattaandrea@gmail.com>
2023-02-01 02:47:07 +0100

    add link to profile page of recent search

--- javascript/searchusers.js
+++ javascript/searchusers.js
@@ -9,15 +9,14 @@ function addRecentSearch(data) {
     const request = data ? "go to profile" : "send request";
     li.innerHTML = `
             <a href="./searchusers.html">${user}</a>
-            <button class="btn btn-primary">${request}</button>
+            <button class="btn btn-primary" onclick="location.href ='../php/userprofile.php?Username=${user}'">${request}</button>
             <button class="btn btn-primary float-end" onclick="deleteSearch(this)">X</button>`;
     query.appendChild(li);
 }
 
 function applyRecentSearch() {
     const user = document.querySelector("#username").value;
-    axios.get('../php/searchusers-api.php', {params: {Username: user}}).then(response => {
-        addRecentSearch(response.data);
-    });
+    axios.get('../php/searchusers-api.php', {params: {Username: user}})
+        .then(response => addRecentSearch(response.data));
 }
 
--- php/userprofile.php
+++ php/userprofile.php
@@ -2,7 +2,7 @@
 require_once "session-check.php";
 $templateParams["js"] = array("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", "../javascript/userprofile.js", "../javascript/image.js");
 if (isset($_GET["Username"])) {
-    $templateParams["title"] = $templateParams["user"]."'s profile";
+    $templateParams["title"] = $_GET["Username"]."'s profile";
 } else {
     throw new Error("You have to specify the user whose profile you want to look at.");
 }
