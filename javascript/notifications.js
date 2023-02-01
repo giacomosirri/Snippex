237890c13761c1b8cb8a569f6dc0eeacc69ad160
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-02-01 17:49:56 +0100

    Improve quality of notifications.js implementation

--- javascript/notifications.js
+++ javascript/notifications.js
@@ -1,72 +1,11 @@
 function markNotificationAsRead(notificationID) {
-    console.log(notificationID);
     axios.put('../php/notifications.php', { NotificationID: notificationID });
-}
-
-function createCommentNotification(data) {
-    return `
-        <div class="notification row align-items-center">
-            <div class="col-md-1"></div>
-            <p class="col-5 col-xl-5 notification-content" style="font-size: 15px; word-spacing: 1px">
-                <strong>~${data["User"]}</strong> has commented your post <em>${data["PostTitle"]}</em>
-            </p>
-            <p class="col-3 col-xl-2 align-self-center notification-date" style="font-size: 80%; color:#6e6e6e">
-                ${data["DateAndTime"]}
-            </p>
-            <button class="btn btn-outline-primary btn-sm col-2 col-xl-1 action" style="font-size: 12px">
-                See comment
-            </button>
-            <div class="col-1 d-flex justify-content-start" style="margin-left: 15px" onclick="markNotificationAsRead(${data["NotificationID"]})">
-                <img class="delete-notification" src="../icons/bin_icon.png" alt="delete notification"/>               
-            </div>
-        </div>
-    `;
-}
-
-function createRatingNotification(data) {
-    return `
-        <div class="notification row align-items-center">
-            <div class="col-md-1"></div>
-            <p class="col-5 col-xl-5 notification-content" style="font-size: 15px; word-spacing: 1px">
-                <strong>~${data["Rater"]}</strong> has rated your post <em>${data["PostTitle"]}</em> with <em>${data["Category"]}</em>
-            </p>
-            <p class="col-5 col-xl-3 align-self-center notification-date" style="font-size: 80%; color:#6e6e6e">
-                ${data["DateAndTime"]}
-            </p>
-            <div class="col-1 d-flex justify-content-start" style="margin-left: 15px" onclick="markNotificationAsRead(${data["NotificationID"]})">
-                <img class="delete-notification" src="../icons/bin_icon.png" alt="delete notification"/>               
-            </div>
-        </div>
-    `;
-}
-
-function createFriendNotification(data) {
-    return `
-        <div class="notification row align-items-center">
-            <div class="col-md-1"></div>
-            <p class="col-5 col-xl-5 notification-content" style="font-size: 15px; word-spacing: 1px">
-                ${data["User"]} has requested to become your friend
-            </p>
-            <p class="col-3 col-xl-2 align-self-center notification-date" style="font-size: 80%; color:#6e6e6e">
-                ${data["DateAndTime"]}
-            </p>
-            <button class="btn btn-outline-primary btn-sm col-2 col-xl-1 action" style="font-size: 12px">
-                Accept friendship
-            </button>
-            <button class="btn btn-outline-primary btn-sm col-2 col-xl-1 action" style="font-size: 12px">
-                Request friendship
-            </button>
-            <div class="col-1 d-flex justify-content-start" style="margin-left: 15px" onclick="markNotificationAsRead(${data["NotificationID"]})">
-                <img class="delete-notification" src="../icons/bin_icon.png" alt="delete notification"/>               
-            </div>
-        </div>
-    `;
 }
 
 function addNotifications(notifications) {
     const section = document.getElementById("all-notifications");
     for (let i=0; i<notifications.length; i++) {
-        section.innerHTML += notifications[i];
+        section.appendChild(notifications[i]);
     }
 }
 
@@ -76,14 +15,84 @@ function countNotifications(data) {
     return notifications;
 }
 
+function addNotificationMainContent(data, type) {
+    const content = document.createElement("a");
+    content.href = "../php/comments.php?PostID=" + `${data["PostID"]}`;
+    content.className = "col-5 col-xl-5 notification-content";
+    content.style.fontSize = "15px";
+    content.style.wordSpacing = "1px";
+    if (type === "comment") {
+        content.innerHTML = `<strong>~${data["User"]}</strong> has commented your post <em>${data["PostTitle"]}</em>`;
+    } else if (type === "rating") {
+        content.innerHTML = `<strong>~${data["Rater"]}</strong> has rated your post <em>${data["PostTitle"]}</em> with <em>${data["Category"]}</em>`;
+    } else {
+        content.innerHTML = `${data["User"]} has requested to become your friend`;
+    }
+    return content;
+}
+
+function addDate(data) {
+    const date = document.createElement("p");
+    date.className = "col-5 col-xl-3 align-self-center notification-date";
+    date.style.fontSize = "80%";
+    date.style.color = "#6e6e6e";
+    date.innerHTML = `${data["DateAndTime"]}`;
+    return date;
+}
+
+function addDeleteNotification(id) {
+    const bin = document.createElement("div");
+    bin.className = "col-1 d-flex justify-content-start";
+    bin.style.marginLeft = "15px";
+    const button = document.createElement("button");
+    bin.appendChild(button);
+    const icon = document.createElement("img");
+    icon.className = "delete-notification";
+    icon.src = "../icons/bin_icon.png";
+    icon.alt = "delete-notification";
+    button.appendChild(icon);
+    button.addEventListener("click", () => markNotificationAsRead(id));
+    return bin;
+}
+
+function addAcceptButton() {
+    const accept = document.createElement("button");
+    accept.className = "btn-primary";
+    accept.innerHTML = `Accept request`;
+    return accept;
+}
+
+function addRejectButton() {
+    const accept = document.createElement("button");
+    accept.className = "btn-danger";
+    accept.innerHTML = `Reject request`;
+    return accept;
+}
+
+function createNotification(data, type) {
+    const notification = document.createElement("div");
+    notification.className = "notification row align-items-center";
+    notification.id = "notification-" + data["NotificationID"];
+    const left_space = document.createElement("div");
+    left_space.className = "col-md-1";
+    notification.appendChild(left_space);
+    notification.appendChild(addNotificationMainContent(data, type));
+    notification.appendChild(addDate(data));
+    if (type === "friendship") {
+        notification.appendChild(addAcceptButton());
+        notification.appendChild(addRejectButton());
+    }
+    notification.appendChild(addDeleteNotification(data["NotificationID"]));
+    return notification;
+}
+
 axios.get('../php/notifications-api.php').then(response => {
-    console.log(response.data);
     const h1 = document.querySelector("header h1");
     const n = countNotifications(response.data);
     h1.innerHTML = `You have ${n} notifications to read`;
     const notifications = [];
-    response.data["comments"].forEach(elem => notifications.push(createCommentNotification(elem)));
-    response.data["ratings"].forEach(elem => notifications.push(createRatingNotification(elem)));
-    response.data["friendships"].forEach(elem => notifications.push(createFriendNotification(elem)));
+    response.data["comments"].forEach(elem => notifications.push(createNotification(elem, "comment")));
+    response.data["ratings"].forEach(elem => notifications.push(createNotification(elem, "rating")));
+    response.data["friendships"].forEach(elem => notifications.push(createNotification(elem, "friendship")));
     addNotifications(notifications);
 });
