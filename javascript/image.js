17c0a0bc7c4c685b283a3a9c2a05f4c2ee2ca97b
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-02-02 10:26:13 +0100

    Fix bug in profile pic display

--- javascript/image.js
+++ javascript/image.js
@@ -1,7 +1,2 @@
-const url_string = window.location.href;
-const url = new URL(url_string);
-const user = url.searchParams.get("Username");
-
-axios.get('../php/image-api.php', {params: {Username: user}}).then(response => {
-    document.getElementById("menu-profile-pic").src = "../profile_pics/" + response.data[0]['ProfilePic'];
-});
+axios.get('../php/image-api.php').then(response => document.getElementById("menu-profile-pic").src =
+    "../profile_pics/" + response.data[0]['ProfilePic']);
--- php/image-api.php
+++ php/image-api.php
@@ -1,7 +1,7 @@
 <?php
 require_once "bootstrap.php";
 global $dbh;
-$user = $_GET["Username"] ?? $_SESSION["LoggedUser"];
+$user = $_SESSION["LoggedUser"];
 $json_data = $dbh->getProfilePic($user);
 header("Content-Type: application/json");
 echo json_encode($json_data);
