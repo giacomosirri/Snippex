7dbbb199640b8aaf3ac37c676da32a767c6f1bea
Luca Pasini <pasini.luca09@gmail.com>
2023-02-02 16:46:39 +0100

    Add jquery

--- php/base.php
+++ php/base.php
@@ -11,6 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Latest compiled JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <!--CSS-->
     <link rel="stylesheet" href="../css/style.css">
