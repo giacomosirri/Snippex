f648daa9ea59cb172c4c564e0f56bbdc5be1f83b
Andrea Zavatta <zavattaandrea@gmail.com>
2023-02-02 19:23:34 +0100

    add search feature in search user page

--- javascript/searchusers.js
+++ javascript/searchusers.js
@@ -1,23 +1,41 @@
 
 window.onload = (event) => {
-    document.getElementById("search-button").addEventListener("click", appendUser);
     document.getElementById("username").addEventListener('input', addProposal);
 };
 
 function deleteSearch(object){
     object.parentNode.style.display='none';
 }
-function addRecentSearch() {
+function addProposal(user) {
+    if(user.data != null && user.data.length > 0){
+        displayProposal();
+    }else{
+        displayRecentSearch();
+    }
+}
+
+function displayProposal(){
     const user = document.querySelector("#username").value;
-    const query = document.querySelector(".container ul");
-    const li = document.createElement("li");
-    li.innerHTML = `
-            <label class="user-label">${user}</label>
-            <button class="delete btn btn-primary float-end">X</button>`;
-    li.getElementsByClassName("user-label")[0]
-        .addEventListener("click", () => window.open(`../php/userprofile.php?Username=${user}`, "_self"));
-    li.getElementsByClassName("delete")[0].addEventListener("click", () => deleteSearch(li));
-    query.appendChild(li);
+    return axios.get('../php/searchusers-api-proposal.php', {params: {Username: user}})
+        .then(response => {
+            document.getElementById("proposes").innerHTML="";
+            return appendUsers(response.data.map(x => x.Username));
+        });
+}
+
+function displayRecentSearch(){
+    axios.get('../php/searchusers-api-recentsearch.php')
+        .then(response =>
+        {
+            console.log(response.data);
+            if(response.data.length > 0) {
+                document.getElementById("proposes").innerHTML="";
+                return appendUsers(response.data);
+            }else{
+                document.getElementById("proposes").innerHTML="";
+                return;
+            }
+        });
 }
 
 function appendUser(user){
@@ -28,16 +46,20 @@ function appendUser(user){
             <span class="input-group-text h-100">&#128269;</span>
          </div>
          <button type="button" class="search-button btn btn-light col-sm-2 sol-ms-2 m-0">${user}</button>`
-    div.querySelector(".search-button").addEventListener("click", () => window.open(`../php/userprofile.php?Username=${user}`, "_self"));
-    document.getElementById("proposes").innerHTML="";
+    div.querySelector(".search-button").addEventListener('click', () => addRecentUser(user))
     document.getElementById("proposes").appendChild(div);
 }
 
-function addProposal() {
-    const user = document.querySelector("#username").value;
-    axios.get('../php/searchusers-api.php', {params: {Username: user}})
-        .then(response => appendUser(response.data[0].Username));
+function appendUsers(users){
+    users.forEach(x => appendUser(x));
 }
+
+function addRecentUser(user){
+    axios.get('../php/searchusers-api-recentsearch.php', {params:{Username:user}});
+}
+
+
+
 
 
 
---
+++ php/searchusers-api-recentsearch.php
@@ -0,0 +1,15 @@
+<?php
+require_once "bootstrap.php";
+global $dbh;
+$username = "";
+if(isset($_GET["Username"])) {
+    $username = $_GET["Username"];
+}
+if(!isset($_SESSION["RecentSearch"])){
+    $_SESSION["RecentSearch"] = array();
+}
+if($username != ""){
+    $_SESSION["RecentSearch"][] = $username;
+}
+echo json_encode($_SESSION["RecentSearch"]);
+?>
