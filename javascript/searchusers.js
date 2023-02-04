84fd65fbc0b951fa2b91e5cc7b17bc0acf9206d9
Andrea Zavatta <zavattaandrea@gmail.com>
2023-02-04 17:29:02 +0100

    change recent users page, improve css

--- javascript/searchusers.js
+++ javascript/searchusers.js
@@ -40,25 +40,57 @@ function displayRecentSearch(){
         });
 }
 
-function appendUser(user){
-    const div = document.createElement("div");
-    div.classList.add("input-group");
-    div.innerHTML =
-        `<div class="input-group-prepend">
-            <img src="../profile_pics/${user}.png">
-         </div>
-         <button type="button" class="search-button btn btn-light col-sm-2 sol-ms-2 m-0">${user}</button>`
+function getPointsFromCategory(stats, category) {
+    for (let i=0; i<stats.length; i++) {
+        if (stats[i]["Category"] === category) {
+            return stats[i]["Points"];
+        }
+    }
+    return 0;
+}
 
-    div.querySelector("img").addEventListener('click', () => {
+function appendUser(user){
+    axios.get('../php/userprofile-api.php', {params: {Username: user}}).then(response => {
+        const numberOfPosts = response.data["user-data"][0]["NumberOfPosts"];
+        const numberOfFriends = response.data["user-data"][0]["NumberOfFriends"];
+        const ratingStats = response.data["rating-stats"];
+        const categories = response.data["categories"];
+        return simpleAppendUser(user, numberOfPosts, numberOfFriends, ratingStats, categories);
+    });
+}
+
+function simpleAppendUser(user, numberOfPosts, numberOfFriend, ratingStats, categories){
+    let div = document.createElement("div");
+    let row = document.createElement("div");
+    let col1 = document.createElement("div");
+    let col2 = document.createElement("div");
+    div.classList.add("container");
+    row.classList.add("row");
+    col1.classList.add("col");
+    col2.classList.add("col");
+    col1.innerHTML+=
+        `<img src="../profile_pics/${user}.png">`;
+    col2.innerHTML+=`
+                <div>${user}</div>
+                <div>post:${numberOfPosts}</div>`;
+    for (let i=0; i<categories.length; i++) {
+        col2.innerHTML+=`<div>${categories[i]["Name"]}:${getPointsFromCategory(ratingStats, categories[i]["Name"])}</div>`;
+    }
+    row.appendChild(col1);
+    row.appendChild(col2);
+    div.appendChild(row);
+
+    row.querySelector("img").addEventListener('click', () => {
         addRecentUser(user);
         window.location.replace("./userprofile.php?Username="+user);
         //change page
     })
+    /*
     div.querySelector(".search-button").addEventListener('click', () => {
         addRecentUser(user);
         window.location.replace("./userprofile.php?Username="+user);
         //change page
-    })
+    })*/
     document.getElementById("proposes").appendChild(div);
 }
 
