b8dd1a682fe550d067850671d25ca82cca2d65c3
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-02-05 16:13:54 +0100

    Fix bug in searchusers-api-recentsearch.php

--- php/searchusers-api-recentsearch.php
+++ php/searchusers-api-recentsearch.php
@@ -4,12 +4,12 @@ global $dbh;
 $username = "";
 if (isset($_GET["Username"])) {
     $username = $_GET["Username"];
-    if (!isset($_SESSION["RecentSearch"])) {
-        $_SESSION["RecentSearch"] = array();
-    }
-    if ($username != "") {
-        $_SESSION["RecentSearch"][] = $username;
-    }
+}
+if (!isset($_SESSION["RecentSearch"])) {
+    $_SESSION["RecentSearch"] = array();
+}
+if ($username != "") {
+    $_SESSION["RecentSearch"][] = $username;
 }
 echo json_encode($_SESSION["RecentSearch"]);
 ?>
