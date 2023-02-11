40751e6b78b5ffc864d71fbfdac4629257f08e66
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-02-11 13:51:52 +0100

    Begin implementation of notifications page style

--- javascript/notifications.js
+++ javascript/notifications.js
@@ -22,24 +22,23 @@ function addNotificationMainContent(data, type) {
     if (type === "comment" || type === "rating") {
         content.href = "../php/comments.php?PostID=" + `${data["PostID"]}`;
     }
-    content.className = "col-5 col-xl-5 notification-content";
-    content.style.fontSize = "15px";
-    content.style.wordSpacing = "1px";
+    content.className = "col-6 notification-content";
     if (type === "comment") {
+        content.classList.add("comment");
         content.innerHTML = `<strong>~${data["User"]}</strong> has commented your post <em>${data["PostTitle"]}</em>`;
     } else if (type === "rating") {
+        content.classList.add("rating");
         content.innerHTML = `<strong>~${data["Rater"]}</strong> has rated your post <em>${data["PostTitle"]}</em> with <em>${data["Category"]}</em>`;
     } else {
-        content.innerHTML = `<strong>${data["Username"]}</strong> has requested to become your friend`;
+        content.classList.add("request");
+        content.innerHTML = `<strong>~${data["Username"]}</strong> has requested to become your friend`;
     }
     return content;
 }
 
 function addDate(data) {
     const date = document.createElement("p");
-    date.className = "col-5 col-xl-3 align-self-center notification-date";
-    date.style.fontSize = "80%";
-    date.style.color = "#6e6e6e";
+    date.className = "col-4 notification-date";
     date.innerHTML = `${data["DateAndTime"]}`;
     return date;
 }
@@ -47,9 +46,8 @@ function addDate(data) {
 function createDeleteNotificationButton(id) {
     const bin = document.createElement("div");
     bin.className = "col-1 d-flex justify-content-start";
-    bin.style.marginLeft = "15px";
     const button = document.createElement("button");
-    button.className = "btn btn-outline-danger";
+    button.className = "btn btn-outline-danger delete-one";
     button.addEventListener("click", () => markNotificationAsRead(id).then(() => location.reload()));
     bin.appendChild(button);
     const icon = document.createElement("img");
@@ -62,13 +60,11 @@ function createDeleteNotificationButton(id) {
 
 function createDeleteAllNotificationsButton(notifications) {
     const header = document.querySelector("header");
-    const div = document.createElement("div");
-    div.className = "col-5 col-md-4 col-lg-3 col-xl-2 d-flex justify-content-center flex-column";
+    const div = document.getElementById("delete-all-div");
     const button = document.createElement("button");
     button.id = "delete-all";
-    button.className = "btn btn-outline-danger btn-sm col-12";
-    button.style.fontSize = "90%";
-    button.innerText = "Delete all notifications";
+    button.className = "btn btn-outline-danger";
+    button.innerText = "Delete all";
     button.addEventListener("click", () => {
         notifications.forEach(notification => markNotificationAsRead(notification.id.split("-")[1]))
         location.reload();
@@ -79,19 +75,19 @@ function createDeleteAllNotificationsButton(notifications) {
 
 function createNotification(data, type) {
     const notification = document.createElement("div");
-    notification.className = "notification row align-items-center";
+    notification.className = "notification row";
     notification.id = "notification-" + data["NotificationID"];
-    const left_space = document.createElement("div");
-    left_space.className = "col-md-1";
-    notification.appendChild(left_space);
     notification.appendChild(addNotificationMainContent(data, type));
     if (type === "friendship") {
+        const div = document.createElement("div");
+        div.className = "col-6";
         const accept = createAcceptFriendshipButton(data["FriendshipID"], data["Username"]);
         accept.addEventListener('click', () => reload());
-        notification.appendChild(accept);
+        div.appendChild(accept);
         const reject = createRejectFriendshipButton(data["FriendshipID"], data["Username"]);
         reject.addEventListener('click', () => reload());
-        notification.appendChild(reject);
+        div.appendChild(reject);
+        notification.appendChild(div);
     } else {
         notification.appendChild(addDate(data));
         notification.appendChild(createDeleteNotificationButton(data["NotificationID"]));
