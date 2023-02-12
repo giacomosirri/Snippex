fa0eaef71771f948113da2d2a32d067054e06c82
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-02-12 10:44:11 +0100

    Redirect user to profile after registration

--- php/register-api.php
+++ php/register-api.php
@@ -15,8 +15,8 @@ if ($password != $passwordConfirm) {
 if (count($dbh->getUserData($username)) == 0) {
     $dbh->registerUser($name, $surname, $username, $dateOfBirth, password_hash($password, PASSWORD_DEFAULT), $signup_date);
     $_SESSION["LoggedUser"] = $username;
-    header("Location: ./feed.php?Username=$username");
-    exit;
+    header("Location: ./profile.php");
+    exit();
 } else {
     throw new Error("Username already exists. Please choose another one.");
 }
